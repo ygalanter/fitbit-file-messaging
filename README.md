bbf318a2eef143052c11d4b28dde375c319972f7
Yuriy Galanter <50464505+YuriGal@users.noreply.github.com>
2022-09-17 13:22:27 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Drop-in replacement for Fitbit peer socket messaging - just replace the imports (in both app and companion)
+```diff
+- import * as messaging from 'messaging';
++ import * as messaging from 'fitbit-file-messaging';
+```
+
+```js
+messaging.peerSocket.onmessage = evt => {
+// this code doesn't change
+}
+```
+
+This messaging uses file transfer under the hood, so it's more reliable than regular messaging.
---
+++ index.js
@@ -0,0 +1,81 @@
+import { inbox, outbox } from 'file-transfer';
+import { encode } from 'cbor';
+
+const MESSAGE_FILE_NAME = 'messaging4d9b79c40abe.cbor';
+
+export const peerSocket = {
+
+    readyState: 0,
+    OPEN: 0,
+
+    addEventListener: function (event, eventHandler) {
+        this[event] = eventHandler;
+    },
+
+    // simulation of `messaging.peerSocket.send` - sends data externally
+    // from device to phone or from phone to device via file transfer
+    send: function (data) {
+        outbox.enqueue(MESSAGE_FILE_NAME, encode(data))
+            .catch(err => {
+                console.error(`#@#@#@#@# Failed to queue '${MESSAGE_FILE_NAME}'. Error: ${err}`);
+            });
+
+    },
+
+    // simulation of `messaging.peerSocket.onMessage` event
+    sendLocal: function (payload) {
+
+        if (this.message) {
+            this.message(payload)
+        } else if (this.onmessage) {
+            this.onmessage(payload)
+        }
+
+    },
+
+    // simulation of `messaging.peerSocket.onMessage` event
+    openLocal: function () {
+        if (this.open) {
+            this.open()
+        } else if (this.onopen) {
+            this.onopen()
+        }
+    }
+
+}
+
+
+if (inbox.pop) { // this is a companion
+
+    inbox.addEventListener("newfile", async () => {
+        let file;
+        let payload = {}
+
+        while ((file = await inbox.pop())) {
+            if (file.name === MESSAGE_FILE_NAME) {
+                payload.data = await file.cbor();
+                peerSocket.sendLocal(payload)
+            }
+        }
+    })
+
+} else { // this is a device
+    const { readFileSync } = require("fs");
+
+    inbox.addEventListener("newfile", async () => {
+        let fileName;
+        let payload = {};
+
+        while (fileName = inbox.nextFile()) {
+            if (fileName === MESSAGE_FILE_NAME) {
+                payload.data = readFileSync(fileName, 'cbor');
+                peerSocket.sendLocal(payload)
+            }
+        }
+    })
+
+}
+
+
+
+setTimeout(() => { peerSocket.openLocal(); }, 1)
